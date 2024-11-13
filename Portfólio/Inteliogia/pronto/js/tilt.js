f2c4e4c1bfa59b19905e25a7b253480cebf2fa31
mathznxs.dev <math983839179@gmail.com>
2024-11-13 13:52:05 -0300

    1

---
+++ Portfólio/Inteliogia/pronto/js/tilt.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelectorAll(".curso-para-box"), {
+    max: 15,
+    speed: 400
+});
+
+VanillaTilt.init(document.querySelectorAll(".aprender-box"), {
+    max: 15,
+    speed: 400
+});
